aefaad3ea82b15d9d029cc053f1ede2ee88aa715
Milo Wical <milo.wical@gmail.com>
2022-08-23 17:50:12 -0400

    Cleaned up testing framework.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,66 +1,34 @@
 #[cfg(test)]
-mod integration_tests {
-  use jwt_cache::JwtCache;
-  use serde::{Deserialize, Serialize};
-  use jsonwebtoken::{encode, decode, get_current_timestamp, Validation, EncodingKey, DecodingKey, Header, Algorithm};
-  use rand::RngCore;
 
-  #[test]
-  fn it_works() {
-      let result = 2 + 2;
-      assert_eq!(result, 4);
-  }
+mod jwt_support;
 
-  #[test]
-  fn instance_test() {
-    let jwt = "".to_string();
+use jwt_cache::JwtCache;
+use serde::{Deserialize, Serialize};
+use jsonwebtoken::{encode, decode, get_current_timestamp, Validation, EncodingKey, DecodingKey, Header, Algorithm};
+use jwt_support::{get_test_jwt};
 
-    JwtCache::new(|| {
-      Some(&jwt)
-    });
-  }
+#[test]
+fn it_works() {
+    let result = 2 + 2;
+    assert_eq!(result, 4);
+}
 
-  #[test]
-  fn refresh_jwt_test() {
-    let jwt_data: (String, Vec<u8>) = get_test_jwt(true);
+#[test]
+fn instance_test() {
+  let jwt = "".to_string();
 
-    let mut jwt_cache = JwtCache::new(|| {
-      Some(&jwt_data.0)
-    });
+  JwtCache::new(|| {
+    Some(&jwt)
+  });
+}
 
-    assert_eq!(jwt_cache.jwt().unwrap(), &jwt_data.0);
-  }
+#[test]
+fn refresh_jwt_test() {
+  let jwt_data: (String, Vec<u8>) = get_test_jwt(true);
 
-  fn get_test_jwt(expired: bool) -> (String, Vec<u8>) {
-    let mut raw_key = vec![0u8; 32];
-    rand::thread_rng().fill_bytes(&mut raw_key);
+  let mut jwt_cache = JwtCache::new(|| {
+    Some(&jwt_data.0)
+  });
 
-    let exp: u64;
-
-    if expired {
-      exp = get_current_timestamp() - 1;
-    }
-    else {
-      exp = get_current_timestamp() + 60;
-    }
-
-
-
-    let claims = TestClaims {
-      exp,
-      sub: "milowical".to_string(),
-      email: "milowical@example.com".to_string()
-    };
-
-    let jwt = encode(&Header::new(Algorithm::HS512), &claims, &EncodingKey::from_secret(&raw_key));
-
-    (jwt.unwrap(), raw_key)
-  }
-
-  #[derive(Debug, Serialize, Deserialize)]
-  struct TestClaims {
-    exp: u64,
-    sub: String,
-    email: String
-  }
+  assert_eq!(jwt_cache.jwt().unwrap(), &jwt_data.0);
 }
---
+++ tests/jwt_support.rs
@@ -0,0 +1,34 @@
+use rand::RngCore;
+use jsonwebtoken::{encode, get_current_timestamp, EncodingKey, Header, Algorithm};
+use serde::{Deserialize, Serialize};
+
+pub fn get_test_jwt(expired: bool) -> (String, Vec<u8>) {
+  let mut raw_key = vec![0u8; 32];
+  rand::thread_rng().fill_bytes(&mut raw_key);
+
+  let exp: u64;
+
+  if expired {
+    exp = get_current_timestamp() - 1;
+  }
+  else {
+    exp = get_current_timestamp() + 60;
+  }
+
+  let claims = TestClaims {
+    exp,
+    sub: "milowical".to_string(),
+    email: "milowical@example.com".to_string()
+  };
+
+  let jwt = encode(&Header::new(Algorithm::HS512), &claims, &EncodingKey::from_secret(&raw_key));
+
+  (jwt.unwrap(), raw_key)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TestClaims {
+  exp: u64,
+  sub: String,
+  email: String
+}
